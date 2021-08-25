38482be311965796504f8a52b0cba3c798628c81
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2021-08-25 10:31:42 -0300

    componente calculator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import "./Calculator.css"
+
+class Calculator extends Component {
+    render() {
+        return(
+            <div className="Calculator">
+                Calculadora
+            </div>
+        )
+    }
+}
+export default Calculator;
