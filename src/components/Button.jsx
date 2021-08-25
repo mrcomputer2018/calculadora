7e464dcc57dd8ecfa56d77ff73b9aec7340de004
mrcomputer2008 <marcelo.ribeiro@proeja.com>
2021-08-25 11:56:33 -0300

    component button #02

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,18 @@ import React from "react"
 import "./Button.css"
 
 const Button = (props) => {
+    let classes = "Button "
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
     return (
-        <button className="Button">{ props.label }</button>
+        <button 
+            className={ classes }
+            /* Conteudo do elemento sera passado para funcao click */
+            onClick={ (event) => props.click && props.click(props.label) }>
+                { props.label }
+        </button>
     )
 }
 export default Button;
